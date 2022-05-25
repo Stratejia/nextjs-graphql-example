6508b0923ef0997832e667751048b550e76fb179
Fabien Roy <fabienroy28@gmail.com>
2022-05-24 22:07:30 -0400

    Add basic usage of styled components (#14)
    * Add basic files for styled components
    
    * Remove import to global styles css
    
    * Add little precommit message
    
    * Fix import
    
    * feat: correctly use styled components
    
    * chore: remove useless config
    
    * Trigger CI

---
+++ frontend/contexts/ThemeProvider.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, ReactNode, useMemo, useState } from 'react';
+import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
+import { ThemeMode } from '../types/styles';
+import { getTheme, GlobalStyle } from '../styles';
+
+const defaultMode = 'light';
+
+type Props = {
+  children: ReactNode;
+};
+
+function ThemeProvider({ children }: Props) {
+  const [mode, setMode] = useState<ThemeMode>(defaultMode);
+
+  const theme = useMemo(() => getTheme(mode), [mode]);
+
+  function switchMode() {
+    setMode(mode === 'light' ? 'dark' : 'light');
+  }
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode, switchMode }}>
+      <StyledComponentsThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </StyledComponentsThemeProvider>
+    </ThemeContext.Provider>
+  );
+}
+
+const ThemeContext = createContext<{
+  mode: ThemeMode;
+  setMode: (mode: ThemeMode) => void;
+  switchMode: () => void;
+}>({ mode: defaultMode, setMode: () => void 0, switchMode: () => void 0 });
+
+export default ThemeProvider;
+
+export { ThemeContext };
---
+++ frontend/types/styles.d.ts
@@ -0,0 +1,3 @@
+declare type ThemeMode = 'light' | 'dark';
+
+export { ThemeMode };
