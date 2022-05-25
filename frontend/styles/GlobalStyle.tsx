ad5d093aaa568bd37bf90a3712a9a1973e1287af
Fabien Roy <fabienroy28@gmail.com>
2022-05-24 21:13:35 -0400

    Remove import to global styles css

--- frontend/styles/GlobalStyle.tsx
+++ frontend/styles/GlobalStyle.tsx
@@ -3,11 +3,21 @@ import { Theme } from './index';
 
 const GlobalStyle = createGlobalStyle(
   ({ theme }) => css`
+    html,
     body {
       margin: 0;
       padding: 0;
       font-family: sans-serif;
       background-color: ${(theme as Theme).colors.background};
+    }
+
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+
+    * {
+      box-sizing: border-box;
     }
   `,
 );
