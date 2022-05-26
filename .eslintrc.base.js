702166496861dd797cb1d704acf621c47333c73e
Fabien Roy <fabienroy28@gmail.com>
2022-05-25 20:39:35 -0400

    chore: remove useless base eslint

--- .eslintrc.base.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rules: {
-    'array-element-newline': ['error',
-      {
-        multiline: false,
-        minItems: 2
-      }]
-  }
-};
--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  extends: ['../.eslintrc.base.js', 'next/core-web-vitals', 'plugin:storybook/recommended'],
+  extends: ['next/core-web-vitals', 'plugin:storybook/recommended'],
   plugins: ['prettier'],
 };
