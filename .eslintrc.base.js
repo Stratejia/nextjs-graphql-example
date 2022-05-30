4a9eb92b4d023af69af48fdca48ca35dd976c01c
Fabien Roy <froy@jstrategia.com>
2022-05-30 19:02:32 -0400

    feat: move most things to eslintrc base

--- .eslintrc.base.js
+++ .eslintrc.base.js
@@ -1,3 +1,18 @@
 module.exports = {
-  // TODO: Remove this file if empty
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+  ],
+  rules: {
+    'array-element-newline': ['error', { minItems: 3 }],
+    'no-restricted-imports': ['warn', { patterns: ['.*'] }],
+    'import/order': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
 };
--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,28 +1,12 @@
 module.exports = {
-  plugins: [
-    '@typescript-eslint',
-    'prettier',
-    'react',
-  ],
+  plugins: ['react'],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+    '../.eslintrc.base',
     'next/core-web-vitals',
-    'plugin:prettier/recommended',
-    'plugin:import/recommended',
-    // TODO: Move the above to .eslint.rc.base
     'plugin:react/recommended',
     'plugin:storybook/recommended',
   ],
   parserOptions: {
     project: ['./tsconfig.json'],
-  },
-  rules: {
-    'array-element-newline': ['error', { minItems: 3 }],
-    'no-restricted-imports': ['warn', { patterns: ['.*'] }],
-    'import/order': 'error',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
   },
 };
