b3fdc7d4e69d3508ee1caeb739f10607c2ae715a
sbouchard-stratejia <113033390+sbouchard-stratejia@users.noreply.github.com>
2022-09-13 16:27:01 -0400

    bugfix: remove warning message when running tests (#63)
    * fix: update jest config and jest setup to remove warning in console when running tests
    
    * fix :  fix linting error

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,7 +1,14 @@
 require('@testing-library/jest-dom');
 
-// eslint-disable-next-line no-undef
 jest.mock('next-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: key => key,
+    };
+  },
+}));
+
+jest.mock('react-i18next', () => ({
   useTranslation: () => {
     return {
       t: key => key,
